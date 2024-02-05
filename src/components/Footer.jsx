b74a8423d18c1f54d6608f3d522afc8a825da736
buburuzaaaaa <andrew4joy2015@gmail.com>
2024-02-05 15:08:52 -0700

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+// Footer.js
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© 2024 Your Website Name. All rights reserved.</p>
+        <div className="social-icons">
+          <a href="#"><i className="fab fa-facebook-f"></i></a>
+          <a href="#"><i className="fab fa-twitter"></i></a>
+          <a href="#"><i className="fab fa-instagram"></i></a>
+          <a href="#"><i className="fab fa-linkedin-in"></i></a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
