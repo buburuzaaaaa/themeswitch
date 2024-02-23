68e0796a1b98d3de1c84ca98e54341b1acde35ab
buburuzaaaaa <andrew4joy2015@gmail.com>
2024-02-23 15:06:31 -0700

    update

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -1,6 +1,29 @@
+import { createContext,useState } from 'react'
+import React from 'react'
+import Header1 from './Header1'
+import Footer from './Footer'
+import Main from './Main'
 
-import { createContext } from 'react';
 
-const ThemeContext = createContext();
 
-export default ThemeContext;
+
+const Them = createContext();
+const ThemeContext = () => {
+  const [themo,setThemo] = useState("light");
+  const handleThemo = () => {
+    if(themo==="light"){
+        setThemo("dark")
+    }else{
+        setThemo("light")
+    }
+}
+  return (
+    <Them.Provider value={{handleThemo, themo}}>
+        <Header1></Header1>
+        <Main></Main>
+        <Footer></Footer>
+    </Them.Provider>
+  )
+}
+
+export {ThemeContext, Them}
